4e5e87330029215881b83e03b5a319285a812c40
UID <UID@web-01-PC>
2019-06-27 17:48:01 +0800

    11

--- jt-common/src/main/java/com/jt/pojo/BasePojo.java
+++ jt-common/src/main/java/com/jt/pojo/BasePojo.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 //pojo基类，完成2个任务，2个日期，实现序列化
-@Data
+@Data//
 @Accessors(chain=true)
 public class BasePojo implements Serializable{
 	private Date created;
